628ef4c18f22f2963d28ba378ee103a924def89e
Alexander Calugari <afishr00@gmail.com>
2019-09-18 15:51:06 +0300

    Refactor triangle

--- triangle/js/resource.js
+++ triangle/js/resource.js
@@ -12,3 +12,34 @@ function loadResource(url) {
 		xhr.send();
 	})
 }
+
+function createShader(gl, type, source) {
+	let shader = gl.createShader(type);
+
+	gl.shaderSource(shader, source);
+	gl.compileShader(shader);
+
+	if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+		alert("Error compiling shader");
+		console.error('Shader error info: ', gl.shaderInfoLog(shader));
+		return false;
+	}
+
+	return shader;
+}
+
+function createProgram(gl, vertexShader, fragmentShader) {
+	let program = gl.createProgram();
+
+	gl.attachShader(program, vertexShader);
+	gl.attachShader(program, fragmentShader);
+	gl.linkProgram(program);
+	gl.validateProgram(program);
+
+	if (!gl.getProgramParameter(program, gl.VALIDATE_STATUS)) {
+		console.error('Error validating program ', gl.getProgramInfoLog(program));
+		return false;
+	}
+
+	return program;
+}
--- triangle/js/webgl.js
+++ triangle/js/webgl.js
@@ -1,9 +1,9 @@
 function initWebGl() {
 	let vertexShaderSource, fragmentShaderSource;
-	loadResource('/shaders/vertexShader.glsl')
+	loadResource('./shaders/vertexShader.glsl')
 	.then((result) => {
 		vertexShaderSource = result;
-		return loadResource('/shaders/fragmentShader.glsl');
+		return loadResource('./shaders/fragmentShader.glsl');
 	})
 	.then((result) => {
 		fragmentShaderSource = result;
@@ -14,9 +14,21 @@ function initWebGl() {
 	})
 }
 
+var gl, program, 
+	vertexArray = [
+	//  X     Y    Z    R    G    B
+		 0.0,  0.5, 0.0, 1.0, 0.0, 0.0,
+		-0.5,  0.0, 0.0, 1.0, 0.0, 0.0,
+		 0.5,  0.0, 0.0, 1.0, 0.0, 0.0,
+
+		-0.5,  0.5, 0.0, 0.0, 1.0, 0.0,
+		 0.5,  0.5, 0.0, 0.0, 1.0, 0.0,
+		 0.0,  0.0,-0.5, 0.0, 1.0, 0.0,
+	];
+
 function startWebGl(vertexShaderSource, fragmentShaderSource) {
 	let canvas = document.getElementById('gl-canvas');
-	let gl = canvas.getContext('webgl');
+	gl = canvas.getContext('webgl');
 
 	if (!gl) {
 		alert('Your browser does not support WebGL. U mad bro');
@@ -27,74 +39,50 @@ function startWebGl(vertexShaderSource, fragmentShaderSource) {
 	canvas.height = gl.canvas.clientHeight;
 	gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
 
-	let vertexShader = gl.createShader(gl.VERTEX_SHADER);
-	let fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
-
-	gl.shaderSource(vertexShader, vertexShaderSource);
-	gl.shaderSource(fragmentShader, fragmentShaderSource);
-
-	gl.compileShader(vertexShader);
-	if (!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS)) {
-		console.error(gl.getShaderInfoLog(vertexShader));
-	}
-	gl.compileShader(fragmentShader);
-	if (!gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS)) {
-		console.error(gl.getShaderInfoLog(fragmentShader));
-	}
+	let vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
+	let fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource);
 	
-	let program = gl.createProgram();
-	gl.attachShader(program, vertexShader);
-	gl.attachShader(program, fragmentShader);
-	gl.linkProgram(program);
-	gl.validateProgram(program);
+	program = createProgram(gl, vertexShader, fragmentShader);
+	
+	draw();
+}
 
-	if (!gl.getProgramParameter(program, gl.VALIDATE_STATUS)) {
-		console.error(gl.getProgramInfoLog(program));
-		return;
-	}
-
+function draw() {
 	let vertexBuffer = gl.createBuffer();
-	gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
 
-	let vertexArray = [
-		0.0, 0.5, 1.0, 0.0, 0.0,
-		0.4, -0.5, 0.0, 1.0, 0.0,
-		-0.4, -0.5, 0.0, 0.0, 1.0
-	];
+	gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
 
 	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertexArray), gl.STATIC_DRAW);
 
-	let positionAttribLocation = gl.getAttribLocation(program, 'vertexPosition');
+	let verticesNumber = vertexArray.length / 6;
 	
+	let positionAttribLocation = gl.getAttribLocation(program, 'vertexPosition');
 	gl.vertexAttribPointer(
-		positionAttribLocation,
-		2,
-		gl.FLOAT,
-		gl.FALSE,
-		5 * Float32Array.BYTES_PER_ELEMENT,
-		0 * Float32Array.BYTES_PER_ELEMENT
-	)
-
+		positionAttribLocation, // ссылка на атрибут
+		3, // кол-во элементов на 1 итерацию
+		gl.FLOAT, // тип данных
+		gl.FALSE, // нормализация
+		6 * Float32Array.BYTES_PER_ELEMENT, // элементов массива на одну вершину
+		0 * Float32Array.BYTES_PER_ELEMENT // отступ для каждой вершины
+	);
 	gl.enableVertexAttribArray(positionAttribLocation);
 
 	var colorAttribLocation = gl.getAttribLocation(program, 'vertexColor');
-
 	gl.vertexAttribPointer(
 		colorAttribLocation, // ссылка на атрибут
 		3, // кол-во элементов на 1 итерацию
 		gl.FLOAT, // тип данных
 		gl.FALSE, // нормализация
-		5 * Float32Array.BYTES_PER_ELEMENT, // элементов массива на одну вершину
-		2 * Float32Array.BYTES_PER_ELEMENT // отступ для каждой вершины
+		6 * Float32Array.BYTES_PER_ELEMENT, // элементов массива на одну вершину
+		3 * Float32Array.BYTES_PER_ELEMENT // отступ для каждой вершины
 	);
-
 	gl.enableVertexAttribArray(colorAttribLocation);
 
 	gl.clearColor(0.75, 0.9, 1.0, 1.0);
-	gl.clear(gl.COLOR_BUFFER_BIT);
-
+	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+	gl.enable(gl.DEPTH_TEST);
 	gl.useProgram(program);
-	gl.drawArrays(gl.TRIANGLES, 0, 3);
+	gl.drawArrays(gl.TRIANGLES, 0, verticesNumber);
 }
 
 document.addEventListener('DOMContentLoaded', () => {
