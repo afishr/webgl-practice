cfe4b0ab728b558afa75f0172b27c30c1aec5b5d
Alexander Calugari <afishr00@gmail.com>
2019-10-04 13:57:23 +0300

    Add GlObject class

--- lab-ii/js/webgl.js
+++ lab-ii/js/webgl.js
@@ -1,3 +1,96 @@
+class GlObject
+{
+	constructor(vertices, indicies, program) {
+		this.program = program;
+
+		this.translation = [0, 0, 0];
+		this.scale = [1, 1, 1];
+		this.rotationAngle = 0;
+		this.rotationMatrix = [1, 1, 1];
+		this.vertices = vertices;
+		this.indicies = indicies;
+
+		this.vertexBuffer = gl.createBuffer();
+		gl.bindBuffer(gl.ARRAY_BUFFER, this.vertexBuffer);
+		gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+
+		this.indexBuffer = gl.createBuffer();
+		gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.indexBuffer);
+		gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indicies), gl.STATIC_DRAW);
+
+		
+		this.identity = new Float32Array(16);
+		this.worldMatrix = new Float32Array(16);
+		this.viewMatrix = new Float32Array(16);
+		this.projMatrix = new Float32Array(16);
+
+		glMatrix.mat4.identity(this.worldMatrix);
+		glMatrix.mat4.identity(this.viewMatrix);
+		glMatrix.mat4.identity(this.projMatrix);
+		glMatrix.mat4.identity(this.identity);
+
+		this.positionAttribLocation = gl.getAttribLocation(program, 'vertexPosition');
+		this.colorAttribLocation = gl.getAttribLocation(program, 'vertexColor');
+
+		this.matWorldUniformLocation = gl.getUniformLocation(program, 'matWorld');
+		this.matViewUniformLocation = gl.getUniformLocation(program, 'matView');
+		this.matProjUniformLocation = gl.getUniformLocation(program, 'matProjection');
+		
+	}
+
+	draw() {
+		glMatrix.mat4.perspective(this.projMatrix, glMatrix.glMatrix.toRadian(45), canvas.clientWidth / canvas.clientHeight, 0.1, 1000.0);
+
+		glMatrix.mat4.lookAt(this.viewMatrix, [0, 0, -15], [0, 0, 0], [0, 1, 0]);
+		glMatrix.mat4.rotate(this.viewMatrix, this.viewMatrix, this.rotationAngle, [0, 1, 0]);
+		glMatrix.mat4.translate(this.worldMatrix, this.identity, this.translation);
+		
+		glMatrix.mat4.rotate(this.worldMatrix, this.worldMatrix, this.rotationAngle, [0, 0, 1]);
+		
+		gl.bindBuffer(gl.ARRAY_BUFFER, this.vertexBuffer);
+		gl.vertexAttribPointer(
+			this.positionAttribLocation, // ссылка на атрибут
+			3, // кол-во элементов на 1 итерацию
+			gl.FLOAT, // тип данных
+			gl.FALSE, // нормализация
+			6 * Float32Array.BYTES_PER_ELEMENT, // элементов массива на одну вершину
+			0 * Float32Array.BYTES_PER_ELEMENT // отступ для каждой вершины
+		);
+		gl.enableVertexAttribArray(this.positionAttribLocation);
+
+		gl.vertexAttribPointer(
+			this.colorAttribLocation, // ссылка на атрибут
+			3, // кол-во элементов на 1 итерацию
+			gl.FLOAT, // тип данных
+			gl.FALSE, // нормализация
+			6 * Float32Array.BYTES_PER_ELEMENT, // элементов массива на одну вершину
+			3 * Float32Array.BYTES_PER_ELEMENT // отступ для каждой вершины
+		);
+		gl.enableVertexAttribArray(this.colorAttribLocation);
+
+		gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.indexBuffer);
+
+		gl.uniformMatrix4fv(this.matWorldUniformLocation, gl.FALSE, this.worldMatrix);
+		gl.uniformMatrix4fv(this.matViewUniformLocation, gl.FALSE, this.viewMatrix);
+		gl.uniformMatrix4fv(this.matProjUniformLocation, gl.FALSE, this.projMatrix);
+
+		gl.drawElements(gl.TRIANGLES, this.indicies.length, gl.UNSIGNED_SHORT, 0);
+	}
+
+	setTranslation(x, y, z)	{
+		this.translation = [x, y, z];
+	}
+
+	setScale(x, y, z)	{
+		this.scale = [x, y, z];
+	}
+
+	setRotation(angle, x, y, z) {
+		this.rotationAngle = angle;
+		this.rotationMatrix = [x, y, z];
+	}
+}
+
 function initWebGl() {
 	let vertexShaderSource, fragmentShaderSource;
 	loadResource('./shaders/vertexShader.glsl')
@@ -14,9 +107,9 @@ function initWebGl() {
 	})
 }
 
-var gl, program, canvas;
+var gl, program, canvas, toDraw = [];
 
-var data = prepareVertices();
+// var data = prepareVertices();
 
 function startWebGl(vertexShaderSource, fragmentShaderSource) {
 	canvas = document.getElementById('gl-canvas');
@@ -31,84 +124,45 @@ function startWebGl(vertexShaderSource, fragmentShaderSource) {
 	canvas.height = window.innerHeight;
 	gl.viewport(0, 0, window.innerWidth, window.innerHeight);
 
+	gl.enable(gl.DEPTH_TEST);
+
+	gl.clearColor(0.75, 0.9, 1.0, 1.0);
+
 	let vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
 	let fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource);
 	
 	program = createProgram(gl, vertexShader, fragmentShader);
-	
-	draw();
+	gl.useProgram(program);
+
+
+	let a = new GlObject(cubeVertices, cubeIndices, program);
+	a.setTranslation(-4, 0, 0);
+
+	let b = new GlObject(crystalVertices, crystalIndices, program);
+	b.setTranslation(0, 0, 0);
+
+	let c = new GlObject(pyramidVertices, pyramidIndices, program);
+	c.setTranslation(4, 0, 0);
+
+	toDraw.push(a, b, c);
+
+	loop();
 }
 
-function draw() {
-	let vertexBuffer = gl.createBuffer();
-	gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(data.mainVertices), gl.STATIC_DRAW);
+angle = 0;
+function loop() {
+	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+	angle = performance.now() / 1000 / 6 * 2 * Math.PI;
 
-	let indexBuffer = gl.createBuffer();
-	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
-	gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(data.mainIndices), gl.STATIC_DRAW);
+	toDraw.forEach(e => {
+		e.setRotation(angle, 0, 1, 0);
+	});
 
-	gl.enable(gl.DEPTH_TEST);
+	toDraw.forEach(e => {
+		e.draw();
+	});
 
-	gl.useProgram(program);
-	
-	let positionAttribLocation = gl.getAttribLocation(program, 'vertexPosition');
-	gl.vertexAttribPointer(
-		positionAttribLocation, // ссылка на атрибут
-		3, // кол-во элементов на 1 итерацию
-		gl.FLOAT, // тип данных
-		gl.FALSE, // нормализация
-		6 * Float32Array.BYTES_PER_ELEMENT, // элементов массива на одну вершину
-		0 * Float32Array.BYTES_PER_ELEMENT // отступ для каждой вершины
-	);
-	gl.enableVertexAttribArray(positionAttribLocation);
-
-	var colorAttribLocation = gl.getAttribLocation(program, 'vertexColor');
-	gl.vertexAttribPointer(
-		colorAttribLocation, // ссылка на атрибут
-		3, // кол-во элементов на 1 итерацию
-		gl.FLOAT, // тип данных
-		gl.FALSE, // нормализация
-		6 * Float32Array.BYTES_PER_ELEMENT, // элементов массива на одну вершину
-		3 * Float32Array.BYTES_PER_ELEMENT // отступ для каждой вершины
-	);
-	gl.enableVertexAttribArray(colorAttribLocation);
-
-	let matWorldUniformLocation = gl.getUniformLocation(program, 'matWorld');
-	let matViewUniformLocation = gl.getUniformLocation(program, 'matView');
-	let matProjUniformLocation = gl.getUniformLocation(program, 'matProjection');
-	let identity = new Float32Array(16);
-	let worldMatrix = new Float32Array(16);
-	let viewMatrix = new Float32Array(16);
-	let projMatrix = new Float32Array(16);
-
-	glMatrix.mat4.identity(worldMatrix);
-	glMatrix.mat4.lookAt(viewMatrix, [0, 0, -8], [0, 0, 0], [0, 1, 0]);
-	glMatrix.mat4.perspective(projMatrix, glMatrix.glMatrix.toRadian(45), canvas.clientWidth / canvas.clientHeight, 0.1, 1000.0);
-
-	gl.uniformMatrix4fv(matWorldUniformLocation, gl.FALSE, worldMatrix);
-	gl.uniformMatrix4fv(matViewUniformLocation, gl.FALSE, viewMatrix);
-	gl.uniformMatrix4fv(matProjUniformLocation, gl.FALSE, projMatrix);
-
-	let angle = 0;
-	glMatrix.mat4.identity(identity);
-
-	function loop() {
-
-		angle = performance.now() / 1000 / 6 * 2 * Math.PI;
-
-		glMatrix.mat4.rotate(worldMatrix, identity, angle, [0, 1, 0]);
-
-		gl.uniformMatrix4fv(matWorldUniformLocation, gl.FALSE, worldMatrix);
-
-		gl.clearColor(0.75, 0.9, 1.0, 1.0);
-		gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-		gl.drawElements(gl.TRIANGLES, data.mainIndices.length, gl.UNSIGNED_SHORT, 0);
-
-		requestAnimationFrame(loop);
-	}
 	requestAnimationFrame(loop);
-
 }
 
 document.addEventListener('DOMContentLoaded', () => {
